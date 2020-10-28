c160a601330616732f7fb9d7f1260a27a95b9b2b
Scott Nicholson <scott.nicholson@live.co.uk>
2020-10-28 22:39:14 +0000

    initial commit

---
+++ src/art-storage/Fire/index.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './style.css';
+
+const Fire = () => {
+  const [isHovered, updateIsHovered] = useState(false);
+
+  return (
+    <div className={`fire ${isHovered && "log-fall"}`} onMouseEnter={() => updateIsHovered(true)} onMouseLeave={() => updateIsHovered(false)}>
+      <div className="log log-front" />
+      <div className="log log-rear" />
+      <div className="log log-bottom" />
+      <div className="flame flame-outer" />
+      <span className="smoke"></span>
+      <span className="smoke smoke-2"></span>
+      <span className="smoke smoke-3"></span>
+    </div>
+  );
+}
+
+export default Fire;
